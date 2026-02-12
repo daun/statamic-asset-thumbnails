5b41777125facfb6525fa6d45ea05b4cef678da3
Philipp Daun <post@philippdaun.net>
2026-02-12 13:08:16 +0100

    Add controller tests
    Signed-off-by: Philipp Daun <post@philippdaun.net>

---
+++ tests/Feature/ControllerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Http\UploadedFile;
+use Statamic\Facades\AssetContainer;
+use Statamic\Facades\User;
+use Facades\Statamic\Version;
+
+beforeEach(function () {
+    if (! config('app.key')) {
+        config(['app.key' => 'base64:'.base64_encode(random_bytes(32))]);
+    }
+
+    Version::shouldReceive('get')->andReturn('3.4.0');
+
+    config(['filesystems.disks.test' => [
+        'driver' => 'local',
+        'root' => $this->getTempDirectory(),
+    ]]);
+
+    $this->image = AssetContainer::make('test')->disk('test')->save()
+        ->makeAsset('one.png')
+        ->upload(UploadedFile::fake()->image('one.png'));
+});
+
+it('denies access when logged out', function () {
+    $this->get('/cp/addons/daun/thumbnails/'.base64_encode('test::one.png'))
+        ->assertForbidden();
+});
+
+it('denies access without permission to view asset', function () {
+    $this->setTestRoles(['test' => ['access cp']]);
+    $user = User::make()->assignRole('test')->save();
+
+    $this->actingAs($user)
+        ->get('/cp/addons/daun/thumbnails/'.base64_encode('test::one.png'))
+        ->assertForbidden();
+});
+
+it('404s when the asset doesnt exist', function () {
+    $this->setTestRoles(['test' => ['access cp', 'view test assets']]);
+    $user = User::make()->assignRole('test')->save();
+
+    $this->actingAs($user)
+        ->get('/cp/addons/daun/thumbnails/'.base64_encode('test::unknown.png'))
+        ->assertNotFound();
+});
+
+it('redirects to placeholder', function () {
+    $this->setTestRoles(['test' => ['access cp', 'view test assets']]);
+    $user = User::make()->assignRole('test')->save();
+
+    $this->actingAs($user)
+        ->get('/cp/addons/daun/thumbnails/'.base64_encode('test::one.png'))
+        ->assertRedirect('https://placehold.co/600?text=Generating\nPreview&font=raleway');
+});
